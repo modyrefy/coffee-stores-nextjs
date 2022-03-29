a5c099414701cbdc90a9cfac19bb5e84f7fa31bf
modyrefy <modyrefay@gmail.com>
2022-03-29 04:32:12 +0400

    create hooks use geolocation create api context

--- store/store-context.tsx
+++ store/store-context.tsx
@@ -4,6 +4,10 @@ import coffeeStoreSlice from "../slice/coffeeStoreSlice";
 export const initialState:AppContextInterface={coffeeStores: null, latLong:"43.65267326999575,-79.39545615725015"};
 //https://hmh.engineering/using-react-contextapi-usereducer-as-a-replacement-of-redux-as-a-state-management-architecture-336452b2930e
 //https://dev.to/bigaru/creating-persistent-synchronized-global-store-using-react-hooks-in-typescript-209a
+//https://shevchenkonik.com/blog/react-typescript-mobx
+//https://felixgerschau.com/react-typescript-context/
+//https://blog.logrocket.com/how-to-use-react-context-typescript/
+//https://gist.github.com/tattali/e56383dbca18322af7a4f3204bd6eddc
 export const ACTION_TYPES = {
     SET_LAT_LONG: "SET_LAT_LONG",
     SET_COFFEE_STORES: "SET_COFFEE_STORES",
@@ -36,10 +40,11 @@ const StoreProvider =({ children }:StoreProviderProps):ReactElement => {
     const [state, dispatch] = useReducer(storeReducer, initialState);
 
     return (
-        <StoreContext.Provider value={{ state, dispatch }}>
+        <StoreContext.Provider value={{ state.initialState, dispatch }}>
     {children}
     </StoreContext.Provider>
-);
-};
+)};
+
+
 
 export default StoreProvider;
